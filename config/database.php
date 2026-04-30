42b364234b678efb8f986ed93bc0f4b012e9fd23
Rinesa <159539787+RinesaIbishi@users.noreply.github.com>
2026-04-30 15:58:33 +0300

    rregullimi i footer-it

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 
 class Database {
     private $host = "localhost";
-    private $db_name = "projekti";
+    private $db_name = "projekti"; // Emri i databazës tënde
     private $username = "root";
     private $password = "";
     public $conn;
@@ -11,20 +11,21 @@ class Database {
         $this->conn = null;
 
         try {
-            // Shtova utf8 që të mos kesh probleme me shkronjat shqipe në DB
             $this->conn = new PDO(
-                "mysql:host=" . $this->host . ";dbname=" . $this->db_name . ";charset=utf8",
-                $this->username,
+                "mysql:host=" . $this->host . ";dbname=" . $this->db_name, 
+                $this->username, 
                 $this->password
             );
 
-           
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             
+
+
         } catch(PDOException $exception) {
-            echo "Gabim në lidhje: " . $exception->getMessage();
+            echo "Lidhja dështoi: " . $exception->getMessage();
         }
 
         return $this->conn;
     }
 }
+?>
